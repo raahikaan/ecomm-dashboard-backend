d8ff3177cb38bd136044d56c3e6afa1ab4bb0a5f
RAahi KaaN <shoaibmkr0@gmail.com>
2022-11-01 11:32:23 -0400

    updated code

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
-const Pool = require('pg').Pool;
+const { Client } = require('pg')
+require('dotenv').config()
 
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "dms",
-    password: "123456",
-    port: 5432
+const client = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dialect: process.env.DB_DIALECT
 })
 
-module.exports = pool;
-
+module.exports = client
